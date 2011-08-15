b776e8fcb9cce8f97e26f23bc1a1250c576f9e08
Carl Brown <carlb@pobox.com>
2011-08-14 19:11:48 -0500

    noop idle dropbox if DROPBOX_HOME isn't set

--- functions.sh
+++ functions.sh
@@ -14,13 +14,16 @@ if [ -z "$SCM_WORKING_DIR" -o ! -d "$SCM_WORKING_DIR" ] ; then
 fi
 
 wait_for_idle_dropbox() {
-	DB_PID="`cat $HOME/.dropbox/dropbox.pid`"
-	
-	#wait for Dropbox to finish syncing anything that might be in progress
-	DB_OPEN_FILES="`/usr/sbin/lsof -p $DB_PID | grep ' REG ' | grep \" ${DROPBOX_HOME}/\" | wc -l`"
 
-	while [ "$DB_OPEN_FILES" -ne 0 ] ; do
-		sleep 3
-		DB_OPEN_FILES="`/usr/sbin/lsof -p $DB_PID | grep ' REG ' | grep \" ${DROPBOX_HOME}/\" | wc -l`"
-	done
+    if [ ! -z "$DROPBOX_HOME" -a -d "$DROPBOX_HOME" ] ; then
+        DB_PID="`cat $HOME/.dropbox/dropbox.pid`"
+        
+        #wait for Dropbox to finish syncing anything that might be in progress
+        DB_OPEN_FILES="`/usr/sbin/lsof -p $DB_PID | grep ' REG ' | grep \" ${DROPBOX_HOME}/\" | wc -l`"
+
+        while [ "$DB_OPEN_FILES" -ne 0 ] ; do
+            sleep 3
+            DB_OPEN_FILES="`/usr/sbin/lsof -p $DB_PID | grep ' REG ' | grep \" ${DROPBOX_HOME}/\" | wc -l`"
+        done
+    fi
 }
