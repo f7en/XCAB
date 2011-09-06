efef90882d609a94d8515d80cafbacb9bc752cee
Carl Brown <carlb@pobox.com>
2011-09-06 12:50:14 -0500

    Stick the branch name in the directory name, so if there is more than one branch being worked on, the user can tell which is which

--- build_and_notify_single_project.sh
+++ build_and_notify_single_project.sh
@@ -135,80 +135,89 @@ for candidate in `git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remote
 			git reset --hard $candidate
 			git clean -d -f -x
 			
+			if [ x"$candidate" != "xmaster" ] ; then
+				#Stick the branch name in the directory name, so if there is more than one branch being worked on, the
+				#  user can tell which is which
+				encoded_candidate="`echo \"${candidate}\" | perl -MURI::Escape -nle 'printf \"%s\", uri_escape($_)'`"
+				build_identifier="${build_time_human}_${encoded_candidate}"
+			else
+				build_identifier="$build_time_human"
+			fi
+			
 			if [ ! -f "$projectFile" ] ; then
 				#no project file, no sense trying to build
-				echo "Could not find project file ${projectFile} for $target on branch ${branch}, not attempting build" >&2
+				echo "Could not find project file ${projectFile} for $target on branch ${candidate}, not attempting build" >&2
 				continue
 			fi
 			
-			mkdir -p "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/"
-			mkdir -p "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+			mkdir -p "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/"
+			mkdir -p "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 			if [ -z "$build_target" ] ; then
 				build_target=`xcodebuild -list -project $projectFile | awk '$1=="Targets:",$1==""' | grep -v "Targets:" | grep -v "^$" | sed -e 's/^  *//' | head -1`
 			fi
 			
-			xcodebuild build -target $build_target -configuration $configuration -project $projectFile -sdk "$use_sdk" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stdout.txt" 2> "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stderr.txt"
+			xcodebuild build -target $build_target -configuration $configuration -project $projectFile -sdk "$use_sdk" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stdout.txt" 2> "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stderr.txt"
 			if [ $? -ne 0 ] ; then
-			  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stdout.txt"
-			  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stderr.txt" >&2
+			  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stdout.txt"
+			  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stderr.txt" >&2
 				echo "Build Failed" >&2
 				echo "Extracting build error" >&2
-				grep ' error: ' "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stdout.txt" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_build_errors.txt"
-				if [ ! -s  "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_build_errors.txt" ] ; then
+				grep ' error: ' "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stdout.txt" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_build_errors.txt"
+				if [ ! -s  "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_build_errors.txt" ] ; then
 					#couldn't find any errors in output, add standard error
-					cp "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stderr.txt" "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_build_errors.txt"
+					cp "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stderr.txt" "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_build_errors.txt"
 				fi
-				ENCODED_ERROR=`perl -MURI::Escape -nle 'printf "%s%%0A", uri_escape($_)' "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_build_errors.txt"`
+				ENCODED_ERROR=`perl -MURI::Escape -nle 'printf "%s%%0A", uri_escape($_)' "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_build_errors.txt"`
 				$my_dir/notify_with_boxcar.sh "notification[message]=Error+building+${target}%0A${ENCODED_ERROR}"
-				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt" #Don't try to build this again - it would fail over and over
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt" #Don't try to build this again - it would fail over and over
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 				exit 3
 			fi
-			cat "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stdout.txt"
-		  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stderr.txt" >&2
+			cat "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stdout.txt"
+		  cat "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stderr.txt" >&2
 		  
 			#Don't build this again this run if more than one branch points at same sha
 			already_built="$already_built $sha"
 			
-			App_location="`grep 'iphoneos.*\\.app\"$' $OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}_xcodebuild_stdout.txt | grep '^ */usr/bin/codesign' | sed -e 's/\"[^\"]*\$//' -e 's/^.*\"//'`"
+			App_location="`grep 'iphoneos.*\\.app\"$' $OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}_xcodebuild_stdout.txt | grep '^ */usr/bin/codesign' | sed -e 's/\"[^\"]*\$//' -e 's/^.*\"//'`"
 			App_name="`echo \"${App_location}\" | sed -e 's,^.*/,,' -e 's/\.app$//'`"
 			
 			echo "Built App named '${App_name}' in relative location '${App_location}'"
 			
 			if [ -d "${App_location}" ] ; then
-				xcrun -sdk iphoneos PackageApplication "${App_location}" -o "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa" --sign "iPhone Developer" --embed "$provprofile"
+				xcrun -sdk iphoneos PackageApplication "${App_location}" -o "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa" --sign "iPhone Developer" --embed "$provprofile"
 			else
 				echo "Could not find app '${App_name}' at relative location '${App_location}' full path '`pwd`/${App_location}'"
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa"
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa"
 				echo "Package Failed" >&2
-				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt" #Don't try to build this again - it would fail over and over
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt" #Don't try to build this again - it would fail over and over
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 				exit 3
 			fi
 			
 			if [ $? -ne 0 ] ; then
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa"
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa"
 				echo "Package Failed" >&2
-				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt" #Don't try to build this again - it would fail over and over
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+				echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt" #Don't try to build this again - it would fail over and over
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 				exit 3
 			fi
 						
 			if [ ! -z "${XCAB_WEB_ROOT}" -a -f "/Applications/BetaBuilder for iOS Apps.app/Contents/MacOS/BetaBuilder for iOS Apps" ] ; then
-				echo "running /Applications/BetaBuilder for iOS Apps.app/Contents/MacOS/BetaBuilder for iOS Apps" -ipaPath="$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa" -outputDirectory="$OVER_AIR_INSTALLS_DIR/$target/$build_time_human" -webserver="${XCAB_WEB_ROOT}/${target}/$build_time_human"
+				echo "running /Applications/BetaBuilder for iOS Apps.app/Contents/MacOS/BetaBuilder for iOS Apps" -ipaPath="$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa" -outputDirectory="$OVER_AIR_INSTALLS_DIR/$target/$build_time_human" -webserver="${XCAB_WEB_ROOT}/${target}/$build_time_human"
 				
-				"/Applications/BetaBuilder for iOS Apps.app/Contents/MacOS/BetaBuilder for iOS Apps" -ipaPath="$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa" -outputDirectory="$OVER_AIR_INSTALLS_DIR/$target/$build_time_human" -webserver="${XCAB_WEB_ROOT}/${target}/$build_time_human"
+				"/Applications/BetaBuilder for iOS Apps.app/Contents/MacOS/BetaBuilder for iOS Apps" -ipaPath="$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa" -outputDirectory="$OVER_AIR_INSTALLS_DIR/$target/$build_time_human" -webserver="${XCAB_WEB_ROOT}/${target}/$build_time_human"
 				if [ $? -ne 0 ] ; then
-					rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa"
+					rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa"
 					echo "BetaBuilder for iOS Apps Failed" >&2
-					echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt" #Don't try to build this again - it would fail over and over
-					rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+					echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt" #Don't try to build this again - it would fail over and over
+					rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 					exit 3
 				fi
 				
 				#Save off the symbols, too
 				if [ -d "${App_location}.dSYM" ] ; then
-					tar czf "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${App_name}.app.dSYM.tar.gz" "${App_location}.dSYM"
+					tar czf "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${App_name}.app.dSYM.tar.gz" "${App_location}.dSYM"
 				fi
 
 				if [ ! -z "$TESTFLIGHT_API_TOKEN" -a ! -z "$TESTFLIGHT_TEAM" ] ; then
@@ -218,12 +227,12 @@ for candidate in `git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remote
 						TESTFLIGHT_NOTIFY="-F notify=False"
 					fi
 					if [ -z "`echo $ignore_opts | grep -i testflight`" ] ; then
-					  curl http://testflightapp.com/api/builds.json -F file=@"$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${App_name}.ipa"  -F api_token="$TESTFLIGHT_API_TOKEN" -F team_token="$TESTFLIGHT_TEAM" -F notes='New ${target} Build was uploaded via the upload API' $TESTFLIGHT_NOTIFY 
+					  curl http://testflightapp.com/api/builds.json -F file=@"$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${App_name}.ipa"  -F api_token="$TESTFLIGHT_API_TOKEN" -F team_token="$TESTFLIGHT_TEAM" -F notes='New ${target} Build was uploaded via the upload API' $TESTFLIGHT_NOTIFY 
 				  fi
 				fi
 				
 				#Clean up temp dir
-				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa"
+				rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa"
 		
 		    if [ -z "`echo $ignore_opts | grep -i rsync`" ] ; then
   				if [ ! -z "$RSYNC_USER" ] ; then
@@ -237,22 +246,22 @@ for candidate in `git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remote
   				wait_for_idle_dropbox
 			
   				#Wait to make sure the file has appeared on the server
-  				IPA_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/${App_name}.ipa\" | grep HTTP/1.1 | awk '{print $2}'`"
-  				PLIST_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/manifest.plist\" | grep HTTP/1.1 | awk '{print $2}'`"
-  				INDEX_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/index.html\" | grep HTTP/1.1 | awk '{print $2}'`"
+  				IPA_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/${App_name}.ipa\" | grep HTTP/1.1 | awk '{print $2}'`"
+  				PLIST_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/manifest.plist\" | grep HTTP/1.1 | awk '{print $2}'`"
+  				INDEX_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/index.html\" | grep HTTP/1.1 | awk '{print $2}'`"
   				RETRY_COUNT=0
   				while [ "$IPA_STATUS" != "200"  -o "$PLIST_STATUS" != "200"  -o "$INDEX_STATUS" != "200" ] ; do
   					#Wait and try again
   					sleep 15
-  					IPA_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/${App_name}.ipa\" | grep HTTP/1.1 | awk '{print $2}'`"
-  					PLIST_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/manifest.plist\" | grep HTTP/1.1 | awk '{print $2}'`"
-  					INDEX_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_time_human/index.html\" | grep HTTP/1.1 | awk '{print $2}'`"
+  					IPA_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/${App_name}.ipa\" | grep HTTP/1.1 | awk '{print $2}'`"
+  					PLIST_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/manifest.plist\" | grep HTTP/1.1 | awk '{print $2}'`"
+  					INDEX_STATUS="`curl -sI -X HEAD \"${XCAB_WEB_ROOT}/${target}/$build_identifier/index.html\" | grep HTTP/1.1 | awk '{print $2}'`"
   					RETRY_COUNT="`expr $RETRY_COUNT + 1`"
   					if [ "$RETRY_COUNT" -gt 30 ] ; then
   						echo "Timeout waiting for web server to become ready" >&2
-  						$my_dir/notify_with_boxcar.sh "notification[source_url]=${XCAB_WEB_ROOT}/$target/$build_time_human/index.html" "notification[message]=ERROR+Timeout+Waiting+For+Webserver+For+${target}+Build"
-  						echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt" #Don't try to build this again - it would generate timeouts over and over if the web server is down
-							rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+  						$my_dir/notify_with_boxcar.sh "notification[source_url]=${XCAB_WEB_ROOT}/$target/$build_identifier/index.html" "notification[message]=ERROR+Timeout+Waiting+For+Webserver+For+${target}+Build"
+  						echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt" #Don't try to build this again - it would generate timeouts over and over if the web server is down
+							rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 							exit 4
   					fi
   				done
@@ -261,15 +270,15 @@ for candidate in `git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remote
   				#  going to be a bunch of new changes per run
   				#   so it won't spam the user to notify for each one
   				#Notify with Boxcar
-  				$my_dir/notify_with_boxcar.sh "notification[source_url]=${XCAB_WEB_ROOT}/$target/$build_time_human/index.html" "notification[message]=New+${target}+Build+available"
+  				$my_dir/notify_with_boxcar.sh "notification[source_url]=${XCAB_WEB_ROOT}/$target/$build_identifier/index.html" "notification[message]=New+${target}+Build+available"
   		  fi
 			else
 				#Put in the permenant location
-				cp "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/${App_name}.ipa" "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${App_name}.ipa"
+				cp "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/${App_name}.ipa" "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${App_name}.ipa"
 				
 				#Save off the symbols, too
 				if [ -d "${App_location}.dSYM" ] ; then
-					tar czf "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${build_target}.app.dSYM.tar.gz" "${App_location}.dSYM"
+					tar czf "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${build_target}.app.dSYM.tar.gz" "${App_location}.dSYM"
 				fi
 				
 				if [ ! -z "$TESTFLIGHT_API_TOKEN" -a ! -z "$TESTFLIGHT_TEAM" ] ; then
@@ -279,18 +288,18 @@ for candidate in `git branch -a | sed -e 's/^..//' -e 's/ ->.*$//' -e 's,^remote
 						TESTFLIGHT_NOTIFY="-F notify=False"
 					fi
 					if [ -z "`echo $ignore_opts | grep -i testflight`" ] ; then
-  				  curl http://testflightapp.com/api/builds.json -F file=@"$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/${App_name}.ipa"  -F api_token="$TESTFLIGHT_API_TOKEN" -F team_token="$TESTFLIGHT_TEAM" -F notes='New ${target} Build was uploaded via the upload API' $TESTFLIGHT_NOTIFY 
+  				  curl http://testflightapp.com/api/builds.json -F file=@"$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/${App_name}.ipa"  -F api_token="$TESTFLIGHT_API_TOKEN" -F team_token="$TESTFLIGHT_TEAM" -F notes='New ${target} Build was uploaded via the upload API' $TESTFLIGHT_NOTIFY 
 				  fi
 				else
-					echo "ipa saved to $OVER_AIR_INSTALLS_DIR/$target/$build_time_human/" >&2
+					echo "ipa saved to $OVER_AIR_INSTALLS_DIR/$target/$build_identifier/" >&2
 					echo "but TestFlight credentials not defined and Beta Builder not installed (or web root not configured)" >&2
 					echo "So it's not very useful, except maybe as a backup for your app builds.  Consider adding one of those services" >&2
 				fi
 				
 			fi
 
-			echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_time_human/sha.txt"
-			rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_time_human/"
+			echo "$sha" > "$OVER_AIR_INSTALLS_DIR/$target/$build_identifier/sha.txt"
+			rm -rf "$OVER_AIR_INSTALLS_DIR/$target/tmp/$build_identifier/"
 		fi
 	fi
 done
